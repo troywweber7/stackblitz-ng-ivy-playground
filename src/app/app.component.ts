63009b1bbb4e91296b1f0f51d29ad04b8f76ab53
troywweber7 <troywweber@gmail.com>
2021-03-18 12:05:19 -0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,9 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  buttons = [
+  readonly newButtonImage =
+    "https://cdn.pixabay.com/photo/2016/10/10/01/49/plus-1727487__340.png";
+  visibleButtons = [
     {
       image:
         "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/44dc8870-2a31-4ffe-a3b8-e3132a11d974/d9td5j6-d68ffb76-ec23-453f-a65a-5329540639e0.png",
@@ -22,6 +24,8 @@ export class AppComponent {
         "https://cdn1.iconfinder.com/data/icons/gamepads-2/100/gamepads_n64-512.png",
       text: "N64"
     },
+  ];
+  buttons = [
     {
       image:
         "https://img.icons8.com/color/452/nintendo-gamecube-controller.png",
@@ -42,15 +46,17 @@ export class AppComponent {
       text: "Switch"
     },
     {
-      image: "https://estadioretro.be/videojuegos/wp-content/uploads/2020/10/gameboy_icon.png",
+      image:
+        "https://estadioretro.be/videojuegos/wp-content/uploads/2020/10/gameboy_icon.png",
       text: "GameBoy"
-    } ,
+    },
     {
       image: "https://static.thenounproject.com/png/44993-200.png",
       text: "GBC"
     },
     {
-      image: "https://www.freeiconspng.com/thumbs/gameboy-icon/gameboy-icon-5.png",
+      image:
+        "https://www.freeiconspng.com/thumbs/gameboy-icon/gameboy-icon-5.png",
       text: "GBA"
     },
     {
@@ -58,12 +64,25 @@ export class AppComponent {
       text: "GBA SP"
     },
     {
-      image: "https://cdn2.iconfinder.com/data/icons/the-evolution-of-consoles-game/64/3Ds_console_game_evolution_copy-512.png",
+      image:
+        "https://cdn2.iconfinder.com/data/icons/the-evolution-of-consoles-game/64/3Ds_console_game_evolution_copy-512.png",
       text: "NDS"
     },
     {
-      image: "https://images-na.ssl-images-amazon.com/images/I/811Ap7oY41L._AC_SL1500_.jpg",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/811Ap7oY41L._AC_SL1500_.jpg",
       text: "3DS"
     }
   ];
+
+  newButton() {
+    if (this.buttons.length)
+      this.visibleButtons.push(this.buttons.pop())
+  }
+
+  removeButton(index: number) {
+    const button = this.visibleButtons.splice(index, 1)[0];
+    this.buttons.push(button);
+    return false;
+  }
 }
