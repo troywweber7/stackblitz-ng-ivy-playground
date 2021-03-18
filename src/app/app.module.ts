79565fd00ebfdb52225d2cb2e5f7497316ce0bb2
troywweber7 <troywweber@gmail.com>
2021-03-18 00:48:21 -0700

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { HomeButtonModule } from "./home-button/home-button.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HomeButtonModule],
+  imports: [BrowserModule, FormsModule, HomeButtonModule, FlexLayoutModule],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent]
 })
